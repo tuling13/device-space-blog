641ce83a2e426a5690f441a082d68b8db7ddfb90
pingdev45 <1747944660@qq.com>
2026-07-06 16:35:15 +0800

    fix router basename and add SPA 404 fallback

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import BlogHome from './pages/BlogHome'
 import LoginPage from './pages/LoginPage'
 
+const basename = import.meta.env.BASE_URL?.replace(/\/$/, '') || '/'
+
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<BlogHome />} />
         <Route path="/login" element={<LoginPage />} />
